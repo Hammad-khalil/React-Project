fc4401feb47499005ffe06eb788df32b72d43746
Hammad Khalil <shhamadkhalil0786@gmail.com>
2024-05-03 07:15:39 +0500

    practicing ways to import and export modules data

---
+++ src/Components/values.js
@@ -0,0 +1,11 @@
+const name = "Hammad";
+const lname = "Khalil";
+const rollno = "21-Arid-874";
+
+let a = function () {
+  let degree = "Software Engineering";
+  return degree;
+};
+
+export default name;
+export { lname, rollno, a };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import name, { lname, rollno, a } from "./Components/values";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <>
+      <App />
+      <li>Hello {name}</li>
+      <li> {lname}</li>
+      <li> {rollno}</li>
+      {a()}
+    </>
   </React.StrictMode>
 );
 
